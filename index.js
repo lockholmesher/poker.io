078624c39bf169280c88f5cf0d5b6124670e027d
Sher Lockholme <lockholme8@gmail.com>
2020-10-28 10:42:32 +0700

    Update

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-const app = express()
-
-app.use(express.static('public'))
-
-var server = app.listen(3000, function() {
-    console.log('Listening on port ' + server.address().port);
-});
